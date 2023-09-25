00e8453172600eafcdb94d48b37e0c2cb43ee006
Nikita Khodov <khodovn@mail.ru>
2023-09-25 13:00:35 +0300

    add improvements

--- Fintech/src/main/java/ru/khodov/Main.java
+++ Fintech/src/main/java/ru/khodov/Main.java
@@ -2,7 +2,7 @@ package ru.khodov;
 
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,52 +11,66 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         List<Weather> weatherList = new ArrayList<>();
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        weatherList.add(new Weather("Москва", -21, dateFormat.parse("2023-12-21 10:00:00")));
-        weatherList.add(new Weather("Воронеж", -20, dateFormat.parse("2023-12-21 10:00:00")));
-        weatherList.add(new Weather("Липецк", 22, dateFormat.parse("2023-09-01 19:00:00")));
-        weatherList.add(new Weather("Москва", 22, dateFormat.parse("2023-09-21 19:00:00")));
-        weatherList.add(new Weather("Елец", 5, dateFormat.parse("2023-11-21 13:00:00")));
-        weatherList.add(new Weather("Волгоград", 30, dateFormat.parse("2023-06-21 15:30:00")));
-
-
-        System.out.println("Среднее значение температуры в регионах: " + averageTemperature(weatherList));
+        weatherList.add(new Weather("Москва", 21.0, LocalDateTime.of(2023, 12, 21, 10, 0, 0)));
+        weatherList.add(new Weather("Воронеж", -20.0, LocalDateTime.of(2023, 12, 21, 10, 0, 0)));
+        weatherList.add(new Weather("Липецк", 22.0, LocalDateTime.of(2023, 9, 1, 19, 0, 0)));
+        weatherList.add(new Weather("Москва", 22.0, LocalDateTime.of(2023, 9, 21, 19, 0, 0)));
+        weatherList.add(new Weather("Елец", 5.0, LocalDateTime.of(2023, 11, 21, 13, 0, 0)));
+        weatherList.add(new Weather("Волгоград", 30.0, LocalDateTime.of(2023, 6, 21, 15, 30, 0)));
 
 
-        System.out.println(temperaturesAreHigherThan(weatherList, -22));
+        for (Map.Entry<String, Double> entry : averageTemperature(weatherList).entrySet()) {
+            String region = entry.getKey();
+            double avrTemperature = entry.getValue();
+
+            System.out.println(region + " средняя температура: " + avrTemperature);
+        }
 
 
-        for (Map.Entry<UUID, List<Integer>> entry : mapIdToTemperature(weatherList).entrySet()) {
+        System.out.println(temperaturesAreHigherThan(weatherList, -22.0));
+
+
+        for (Map.Entry<UUID, List<Double>> entry : mapIdToTemperature(weatherList).entrySet()) {
             UUID regionId = entry.getKey();
-            List<Integer> temperatures = entry.getValue();
+            List<Double> temperatures = entry.getValue();
 
             System.out.println("RegionId " + regionId + ": " + temperatures);
         }
 
 
-        for (Map.Entry<Integer, List<Weather>> entry : mapTemperatureToObjects(weatherList).entrySet()) {
-            int temperature = entry.getKey();
+        for (Map.Entry<Double, List<Weather>> entry : mapTemperatureToObjects(weatherList).entrySet()) {
+            double temperature = entry.getKey();
             List<Weather> object = entry.getValue();
 
             System.out.println("Температура " + temperature + ": " + object);
         }
     }
 
-    private static double averageTemperature(List<Weather> weatherList) {
-        return weatherList.stream().mapToDouble(Weather::getTemperature).average().orElse(0.0);
+    private static Map<String, Double> averageTemperature(List<Weather> weatherList) {
+        return weatherList.stream().
+                collect(
+                        Collectors.groupingBy(
+                                Weather::getRegionName,
+                                Collectors.averagingDouble(
+                                        Weather::getTemperature
+                                )
+                        )
+
+                );
     }
 
-    private static List<String> temperaturesAreHigherThan(List<Weather> weatherList, int desiredTemperature) {
+
+    private static List<String> temperaturesAreHigherThan(List<Weather> weatherList, double desiredTemperature) {
         return weatherList.stream().filter(weather -> weather.getTemperature() > desiredTemperature).map(Weather::getRegionName).distinct().toList();
     }
 
 
-    private static Map<UUID, List<Integer>> mapIdToTemperature(List<Weather> weatherList) {
+    private static Map<UUID, List<Double>> mapIdToTemperature(List<Weather> weatherList) {
         return weatherList.stream()
                 .collect(
                         Collectors.groupingBy(
@@ -69,7 +83,7 @@ public class Main {
                 );
     }
 
-    private static Map<Integer, List<Weather>> mapTemperatureToObjects(List<Weather> weatherList) {
+    private static Map<Double, List<Weather>> mapTemperatureToObjects(List<Weather> weatherList) {
         return weatherList.stream()
                 .collect(
                         Collectors.groupingBy(

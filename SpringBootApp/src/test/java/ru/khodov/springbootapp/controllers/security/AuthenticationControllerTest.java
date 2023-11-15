2ec29c3e910cfca9a123e31a0dbcf8ed3f7739f8
Nikita Khodov <khodovn@mail.ru>
2023-11-15 20:20:00 +0300

    Исправление дз8

--- SpringBootApp/src/test/java/ru/khodov/springbootapp/controllers/security/AuthenticationControllerTest.java
+++ SpringBootApp/src/test/java/ru/khodov/springbootapp/controllers/security/AuthenticationControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.khodov.springbootapp.SpringBootAppApplicationTests;
@@ -25,7 +24,7 @@ public class AuthenticationControllerTest extends SpringBootAppApplicationTests 
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean
+    @Autowired
     private UserJpaRepository userRepository;
 
     @BeforeEach
--- SpringBootApp/src/test/java/ru/khodov/springbootapp/controllers/security/WeatherControllerTest.java
+++ SpringBootApp/src/test/java/ru/khodov/springbootapp/controllers/security/WeatherControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -37,7 +36,7 @@ public class WeatherControllerTest extends SpringBootAppApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean
+    @Autowired
     private WeatherService weatherService;
 
     @Autowired
